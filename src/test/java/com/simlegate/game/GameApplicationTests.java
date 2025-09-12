325b6e0b7aff7f1c54dd90cea14c927af7a43efd
longph.hcm <longph.hcm@vnpt.vn>
2025-09-12 16:46:27 +0700

    start project

---
+++ src/test/java/com/simlegate/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simlegate.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
